2a619fe9c1872cb03c14eb0eb55880cb16eb58a7
German Zverev <German.soft@mail.ru>
2026-05-09 23:17:04 +0300

    Added center crop for inference

--- src/transforms/audio_cutter.py
+++ src/transforms/audio_cutter.py
@@ -9,14 +9,13 @@ class AudioCutter:
     def __call__(self, audio):
         cur_size = audio.shape[-1]
 
-        if not self.is_train:
-            return audio
-
         if cur_size < self.target_size:
             pad_size = self.target_size - cur_size
-            audio = F.pad(audio, (0, pad_size), mode = 'replicate')
-        else:
+            return F.pad(audio, (0, pad_size), mode = 'replicate')
+        
+        if self.is_train:
             start = torch.randint(0, cur_size - self.target_size + 1, (1,)).item()
-            audio = audio[:, start : start + self.target_size]
+        else:
+            start = (cur_size - self.target_size) // 2
 
-        return audio
+        return audio[:, start : start + self.target_size]
