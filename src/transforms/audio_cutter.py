5aa161cfb94a69aefc7d24b1b6db48be29cfbc5c
German Zverev <German.soft@mail.ru>
2026-05-09 16:59:58 +0300

    Fixed dataset and added crop

---
+++ src/transforms/audio_cutter.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as F
+
+class AudioCutter:
+    def __init__(self, target_sr = 16000, duration = 0.5, is_train = True):
+        self.target_size = int(self.target_sr * duration)
+        self.is_train = is_train
+
+    def __call__(self, audio):
+        cur_size = audio.shape[-1]
+
+        if cur_size < self.target_size:
+            pad_size = self.target_size - cur_size
+            audio = F.pad(audio, (0, pad_size), mode = 'replicate')
+        else:
+            start = torch.randint(0, cur_size - self.target_size + 1, (1,)).item()
+            audio = audio[:, start : start + self.target_size]
+
+        return audio
